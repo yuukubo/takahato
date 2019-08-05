b3cab68fc9093e65e617ac31598f5b35077c1e50
M.Tamura <tamura2004@gmail.com>
2019-08-05 18:58:37 +0900

    Update plant trait

--- lib.js
+++ lib.js
@@ -10,16 +10,8 @@ class Creature {
     this.path = [];
     this.creatures = creatures;
   }
-  update() {
-    this.r++;
-    // 5%の確率で分裂
-    if (random() < 0.02) {
-      this.r = R;
-      this.creatures.push(new Creature(R, this.p, this.creatures));
-      while (this.creatures.length > POPULATION) {
-        this.creatures.shift();
-      }
-    }
+  update(){
+    // must be override
   }
   move() {
     if (this.path.length > 0) {
@@ -40,5 +32,41 @@ class Creature {
   draw() {
     fill('#56a764');
     circle(this.p.x, this.p.y, this.r);
+  }
+}
+
+class Plant extends Creature {
+  constructor(r, p, creatures) {
+    super(r, p, creatures);
+
+    let d = p5.Vector.random2D().mult(random(RANGE));
+    let q = p5.Vector.add(this.p, d);
+    while (q.x < R || windowWidth - R < q.x || q.y < R || windowHeight - R < q.y) {
+      d.rotate(random());
+      q = p5.Vector.add(this.p, d);
+    }
+
+    const STEP = int(this.p.dist(q) / MOVE);
+    for (let i = 1; i <= STEP; i++) {
+      const x = i / STEP;
+      this.path.push(p5.Vector.lerp(this.p, q, easing(x)));
+    }
+  }
+  update() {
+    if (this.r < 50) {
+      this.r++;
+    }
+    // 5%の確率で分裂
+    if (random() < 0.005) {
+      this.creatures.push(new Plant(R, this.p, this.creatures));
+      while (this.creatures.length > POPULATION) {
+        this.creatures.shift();
+      }
+    }
+  }
+  move() {
+    if (this.path.length > 0) {
+      this.p = this.path.shift();
+    }
   }
 }
--- sketch.js
+++ sketch.js
@@ -1,14 +1,16 @@
-// タカハトゲームのシミュレータ
+// 食物連鎖のシミュレータ
 const R = 5;
-const MOVE = 2;
-const POPULATION = 500;
+const RR = 10;
+const MOVE = 8;
+const POPULATION = 100;
+const RANGE = 200; // 種子が飛散する最大距離
 
 let creatures = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   const p = createVector(windowWidth / 2, windowHeight / 2);
-  creatures.push(new Creature(R, p, creatures));
+  creatures.push(new Plant(R, p, creatures));
 }
 
 function draw() {
