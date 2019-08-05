7416cc2a4490b54724dd0d11f589ca62fef96c07
M.Tamura <tamura2004@gmail.com>
2019-08-05 15:12:18 +0900

    A trailing shadow

--- lib.js
+++ lib.js
@@ -7,15 +7,16 @@ class Creature {
     // 画面中央
     this.p = createVector(windowWidth / 2, windowHeight / 2);
     this.path = [];
-    this.wait = 10;
+    this.wait = [];
   }
   move() {
     if (this.path.length > 0) {
       // １フレーム移動
       this.p = this.path.shift();
-    } else if (this.wait > 0) {
+      this.wait.push(this.p);
+    } else if (this.wait.length > 0) {
       // その場で待つ
-      this.wait--;
+      this.wait.shift();
     } else {
       // STEPフレームの移動を決める
       const x = random(R, windowWidth - R);
@@ -25,12 +26,13 @@ class Creature {
         const x = i / STEP;
         this.path.push(p5.Vector.lerp(this.p, q, easing(x)));
       }
-      // 待ち時間を決める
-      this.wait = random(MAX_WAIT);
     }
   }
   draw() {
     line(0, 0, this.p.x, this.p.y);
+    for (const w of this.wait) {
+      circle(w.x, w.y, 100);
+    }
     circle(this.p.x, this.p.y, 100);
   }
 }
