658d1707d97ec5da75e06f7209f610e5157c687e
M.Tamura <tamura2004@gmail.com>
2019-08-07 15:25:56 +0900

    Update plant parametor

--- sketch.js
+++ sketch.js
@@ -1,10 +1,10 @@
 // 食物連鎖のシミュレータ
 const INIT = 16; // 生物の初期サイズ
-const MIN = 8; // 生存最小サイズ
+const MIN = 12; // 生存最小サイズ
 const MAX = 128; // 成熟サイズ
 const NEAR = 100; // 隣接判定距離
 const DENSITY = 8; // 共存可能個体数
-const RANGE = 150; // 種子が飛散する最大距離
+const RANGE = 300; // 種子が飛散する最大距離
 
 let plants = new Plants();
 
