47f18578db016d4a6eea41e7bf46ec6cc317eb8a
yuukubo <i.yusuke.shk@gmail.com>
2019-08-22 00:48:19 +0900

    keyIsDown test

--- sketch.js
+++ sketch.js
@@ -1,15 +1,34 @@
 // stg
 
-let [x, y, vx, vy] = [10, 0, 2, 2];
+let [x, y, vx, vy, canvasx, canvasy] = [10, 0, 2, 2, 400, 500];
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(canvasx, canvasy);
 }
 
 function draw() {
   background(0);
- if (windowHeight < y) { y = 0 , x = random( 0 , windowWidth ) }
+  if (x < 0) {x = 0}
+  if (canvasx < x) {x = canvasx}
+  if (canvasy < y) { y = 0 , x = random( 0 , canvasx ) }
   y += vy;
+ 
+  if (keyIsDown(LEFT_ARROW)) {
+    if (0 < x) {x -= 5}
+  }
+
+  if (keyIsDown(RIGHT_ARROW)) {
+    if (x < canvasx) {x += 5}
+  }
+
+  if (keyIsDown(UP_ARROW)) {
+　  if (0 < y) {y -= 5}
+  }
+
+  if (keyIsDown(DOWN_ARROW)) {
+    if (y < canvasy) {y += 5}
+  }
+ 
   fill(255, 255, 1);
   circle(x, y, 10);
 }
