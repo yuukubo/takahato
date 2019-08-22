d518d78b66b7b06a13dddd6472288bd415b2c010
yuukubo <i.yusuke.shk@gmail.com>
2019-08-23 04:47:19 +0900

    array test

--- sketch.js
+++ sketch.js
@@ -1,18 +1,29 @@
 // stg
 
-let [x, y, vx, vy, canvasx, canvasy] = [10, 0, 2, 2, 400, 500];
+let [canvasx, canvasy] = [400, 500];
+let stars = new Array(4);
 
 function setup() {
   createCanvas(canvasx, canvasy);
+  background(0);
+  for (var i = 0; i < stars.length; i++) {
+    stars[i] = new Star();
+  }
 }
 
 function draw() {
-  background(0);
-  if (x < 0) {x = 0}
-  if (canvasx < x) {x = canvasx}
-  if (canvasy < y) {y = 0 , x = random(0 , canvasx)}
-  y += vy;
- 
+//  keychk();
+  for (var i = 0; i < stars.length; i++) {
+    stars[i].shootingstar();
+//    stars[i].limitchk();
+//    if (stars[i].vanishing_starlight === 1) {
+//      stars[i].splice(index, i);
+//      stars.push(new Star());
+//    }
+  }
+}
+/*
+function keychk() {
   if (keyIsDown(LEFT_ARROW)) {
     if (0 < x) {x -= 5}
   }
@@ -28,14 +39,41 @@ function draw() {
   if (keyIsDown(DOWN_ARROW)) {
     if (y < canvasy) {y += 5}
   }
- 
-  fallball();
 }
+*/
 
-function fallball() {
-  let star_R = (100 + x) % 255
-  let star_G = y % 255
-  let star_B = (x + y) % 255
-  fill(star_R, star_G, star_B);
-  circle(x, y, 10);
+class Star {
+  constructor() {
+    this.spd = random(1,6);
+    this.location = createVector(random(canvasx), random(canvasy));
+    this.position = createVector(random(canvasx), random(canvasy));
+    this.star_R = random(0,255);
+    this.star_G = random(0,255);
+    this.star_B = random(0,255);
+    this.star_x = 0;
+    this.star_y = 0;
+    let vanishing_starlight = 0;
+    }
+
+  shootingstar() {
+//    this.star_R = (this.star_R + this.location.x) % 255
+//    this.star_G = (this.star_G + this.location.y) % 255
+//    this.star_B = (this.star_B + this.location.x + this.location.y) % 255
+//    this.star_x = this.location.x + 1 * this.spd;
+//    this.star_y = this.location.y + 1 * this.spd;
+    this.position.x = this.position.x + 1 * this.spd;
+    this.position.y = this.position.y + 1 * this.spd;
+//    this.location.add(this.star_x,this.star_y);
+    fill(this.star_R, this.star_G, this.star_B);
+    circle(this.position.x, this.position.y, 10);
+  }
+
+//  limitchk() {
+//    if (this.location.x < 0 || canvasx < this.location.x) {this.vanishing_starlight = this.vanishing}
+//    if (this.location.y < 0 || canvasy < this.location.y) {this.vanishing_starlight = this.vanishing}
+//  }
+
+//  vanishing() {
+//    return 1;
+//  }
 }
